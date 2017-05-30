cb951d2645980622ea4ff14c8013334f18609d28
Diego Fortes <diego.fortes@fullsix.local>
2017-05-30 19:14:56 +0100

    minor changes

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,15 +1,26 @@
-
+/* Words */
 var ps = document.querySelectorAll(".hidden");
 var pWatchers = [];
-var articleWatchers = [];
 for(let i = 0; i < ps.length; i++) {
   pWatchers.push(scrollMonitor.create(ps[i]))
-}
-for(let i = 0; i < articles.length; i++) {
-  articleWatchers.push(scrollMonitor.create(articles[i]))
 }
 pWatchers.forEach(function(watcher, index) {
   watcher.enterViewport(function() {
     ps[index].classList.remove("hidden")
+  })
+});
+
+/* Projects animation */
+
+var articles = document.querySelectorAll(".project-name");
+var articleWatchers = [];
+
+for(let i = 0; i < articles.length; i++) {
+  articleWatchers.push(scrollMonitor.create(articles[i]))
+}
+articleWatchers.forEach(function(watcher, index) {
+  watcher.enterViewport(function() {
+    articles[index].style.transform = "translateY(0)";
+    articles[index].style.opacity = 1;
   })
 });
