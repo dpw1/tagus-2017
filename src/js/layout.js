9ad6c44e43404824988c30863bc7847bb37eb0a3
dpw1 <diego.boarutto.fortes@gmail.com>
2017-06-11 23:49:14 +0100

    added pages, fixed bugs

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,8 +5,8 @@ var elementWatcher = scrollMonitor.create( el );
 $( document ).ready(function() {
 
   elementWatcher.enterViewport(function() {
-    var anim = anime({
-      targets: '.layout__image',
+    var left = anime({
+      targets: '.layout__image--left',
       translateX: [
          { value: '-100vw', duration: 0, delay: 0, elasticity: 0 },
          { value: 0, duration: 2000, delay: 500, elasticity: 0 }
@@ -18,6 +18,20 @@ $( document ).ready(function() {
         easing: 'easeOutExpo',
     });
 
+    var right = anime({
+      targets: '.layout__image--right',
+      translateX: [
+         { value: '200vw', duration: 0, delay: 0, elasticity: 0 },
+         { value: 0, duration: 2000, delay: 500, elasticity: 0 }
+       ],
+       opacity: [
+          { value: 0, duration: 0, delay: 0, elasticity: 0 },
+          { value: 1, duration: 5000, delay: 500, elasticity: 0 }
+        ],
+        easing: 'easeOutExpo',
+    });
+
+      this.destroy();
   });
 
 
